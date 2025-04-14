5727a4baaf4870a6735938fa1f00ed123b420ea2
saumya9889 <msaumya114@gmail.com>
2025-04-14 23:10:22 +0530

    updated

--- frontend/src/components/common/sideBar.jsx
+++ frontend/src/components/common/sideBar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { FaCalendarAlt, FaCog, FaClock } from "react-icons/fa";
 import { TbCalendarEvent } from "react-icons/tb";
-
+import profileImage from "../../assets/images/display.avif";
 
 const Sidebar = () => {
+  const userData = JSON.parse(localStorage.getItem("userData")) || {};
+  const displayName = userData.username || userData.firstName || "User";
+  
   return (
     <aside className="sidebar">
       <div className="logo">CNCT</div>
@@ -15,19 +18,17 @@ const Sidebar = () => {
           <li>
             <FaCalendarAlt /> Bookings
           </li>
-                            
           <li>
-                <FaClock /> Availability
+            <FaClock /> History
           </li>
           <li>
             <FaCog /> Settings
           </li>
         </ul>
       </nav>
-      <button className="create-btn">+ Create</button>
-      <div className="user">
-        <img src="https://via.placeholder.com/40" alt="User" />
-        <span>sarthak pal</span>
+      <div className="profile-section">
+        <img src={profileImage} alt="Profile" className="profile-image" />
+        <span className="username">{displayName}</span>
       </div>
     </aside>
   );
